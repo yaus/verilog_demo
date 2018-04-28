cfc348ac3156eda54abaf8b402249cf486a8a737
YauS <yaus@users.noreply.github.com>
2018-04-28 21:51:24 +0800

    Update test_top.v

--- demo_002/test_top.v
+++ demo_002/test_top.v
@@ -1,4 +1,7 @@
 // Set simulation timestep
+// `timescale a/b 
+// a -- display/printing precision
+// b -- simulation precision
 `timescale 1ns/1ns
 
 // Declare testbench module
@@ -25,12 +28,18 @@ end
 initial begin
     // initial reset signal to logic 1
     rst_n = 0;
+    // #a
+    // a -- time without unit (result time = timescale*a)
+    // #bT 
+    // bT -- time with unit (result time = bT)
     #17ns
+    // = -- blocking assignment
     rst_n = 1;
 end
 
 // Dump waveform
 initial begin
+    // internal function to declare variable dumping scope
     $dumpvars(0);
     #10000ns
     $finish;
@@ -40,8 +49,14 @@ end
 wire clk_o;
 
 // Declare Device Under Test (DUT)
+// AAA BBB ();
+// AAA -- module name
+// BBB -- instance name
 CLK_DIV_2 DUT
 (
+    // .a (b)
+    // a -- DUT's pin name
+    // b -- this module's signal name
     .clk    (clk),
     .rst_n  (rst_n),
     .clk_o  (clk_o)
