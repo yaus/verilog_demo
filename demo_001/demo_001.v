2621f5008a9754ba6ed7d34764fda57cff5185e0
YauS <yaus@users.noreply.github.com>
2018-04-28 21:37:10 +0800

    Update demo_001.v

--- demo_001/demo_001.v
+++ demo_001/demo_001.v
@@ -1,9 +1,20 @@
+// module -- Keyword for module declaration
+// main   -- name of the module
 module main;
+
+// initial -- execute next block/statement once only on start of the simulation
 initial
-    begin
+    // begin -- start a statement block
+    // BLOCK_NAME -- label of statement block
+    begin : BLOCK_NAME
+        // $display -- internal function for print string
         $display("Hello world!");
-	$finish;
-    end
+        // $finish -- internal function for stop simulation
+        $finish;
+    end : BLOCK_NAME
+    // end -- end of a statement block
+    // BLOCK_NAME -- for paired up BEGIN-END(non-necessary)
 
 endmodule
+// endmodule -- Keyword for the end of the module declaration
 
